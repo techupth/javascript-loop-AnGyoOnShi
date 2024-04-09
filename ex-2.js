c8108e5bee9da10b13a3360eefca420c808e91f4
PARN <thehamter123@hotmail.com>
2024-04-09 14:15:59 +0700

    resolve: ex1-6

--- ex-2.js
+++ ex-2.js
@@ -2,5 +2,7 @@
 let numbers = [10, 20, 30, 40, 50];
 let total = 0;
 // Start coding here
-
+for (let checker of numbers) {
+  total = total + checker;
+}
 console.log(total);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let checker of companyName) {
+  reversedCompanyName = checker + reversedCompanyName;
+}
 
 console.log(reversedCompanyName);
